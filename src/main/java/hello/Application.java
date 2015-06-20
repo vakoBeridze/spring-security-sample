daad53ea3df4e062e524b752c0de3f76de794c9a
Vako Beridze <vako.beridze@gmail.com>
2015-06-20 13:52:55 +0400

    added project

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vako on 6/20/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
